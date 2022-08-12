5ab892124c0bec2819ae57be966994c2bcb69e0b
Deepmarik <deepmarik@gmail.com>
2022-08-12 15:58:06 +0300

    task 2.1 is done

--- less-2.php
+++ less-2.php
@@ -18,18 +18,17 @@ do { $answerUser = (int)readline($text);
 } while (true); 
   
   
-
 //Задание 2
 $name1 = readline("Привет, как тебя зовут?\n");
-$objective = readline("Что сегодня нужно сделать?\n");
-for ($i=0;$i< $objective; $i++){}
-$time = (int)readline("Сколько времени это займет?\n");
-$objective1 = readline("А что еще сегодня нужно сделать?\n");
-$time1 = (int)readline("Сколько времени это займет?\n");
-$objective2 = readline("И последнее что сегодня нужно сделать?\n");
-$time2 = (int)readline("Сколько времени это займет?\n");
-$sum=$time+ $time1+$time2;
-echo "$name1, у тебя сегодня три задачи, а именно:\n  - $objective ({$time}ч)\n - $objective1 ({$time}ч)\n - $objective2 ({$time}ч)\n ";
-echo "$name1, это займет у тебя $sum ч\n ";
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
+$numberTasks= (int)readline("Сколько задач на сегодня? - ");
+ for ($i=0; $i < $numberTasks; $i++){
+
+    $objective = readline("Что сегодня нужно сделать?\n");
+    $time = (int)readline("Сколько времени это займет?\n");
+    $sum +=$time;
+    $planDay .= $objective . ' ';
+ }
+ echo PHP_EOL;
+ echo $sum; 
+ echo PHP_EOL;
+ echo $planDay;
